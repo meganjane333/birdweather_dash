f89049b97b1d0e6afb336b02cf0b992d7afcf73a
meganjane333 <megan333jane@gmail.com>
2026-09-24 09:29:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ def display_bird_card(bird, rank):
             if pd.notna(bird.image_url) and bird.image_url:
                 st.image(
                     bird.image_url,
-                    width=80
+                    width=85
                 )
             else:
                 st.markdown("🐦")
